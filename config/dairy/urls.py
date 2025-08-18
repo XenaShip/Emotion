b37467372da4085ff857e9ceb98268adda2da518
XenaShip <shipxena@gmail.com>
2025-08-18 19:50:09 +0300

    temlates+forms

--- config/dairy/urls.py
+++ config/dairy/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import NoteListView
+from .views import NoteListView, NoteCreateView
 
 app_name = 'dairy'  # ✅ это важно!
 
 urlpatterns = [
     path('', NoteListView.as_view(), name='note-list'),
+    path('create/', NoteCreateView.as_view(), name='note-create'),
+    path('delete/', NoteDeleteView.as_view(), name='note-delete'),
+
 ]
--- config/dairy/views.py
+++ config/dairy/views.py
@@ -2,9 +2,21 @@ from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy
 from django.views import generic
 
+from .forms import NoteForm
 from .models import Note
 
 
 class NoteListView(generic.ListView):
     model = Note
     template_name = 'dairy/index.html'
+
+
+class NoteCreateView(generic.CreateView):
+    model = Note
+    form = NoteForm
+    template_name = 'dairy/create_note.html'
+
+
+class NoteDeleteView(generic.DeleteView):
+    model = Note
+    template_name = 'dairy/delete_note.html'
